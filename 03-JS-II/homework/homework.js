6fe40ab5bec3c90aa957fbdba6822bea8f62b54e
fsr1410 <faq.ramos.14101999@gmail.com>
2021-12-03 13:34:50 -0300

    finish 03-JS-II-homework.js

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,12 +5,18 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  if(x>=y)
+    return x;
+  return y;
 }
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+  if(edad>=18)
+    return "Allowed";
+  return "Not allowed";
 }
   
 function conection(status) {
@@ -19,6 +25,11 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+  switch(status){
+    case 1: return "Online";
+    case 2: return "Away";
+    default: return "Offline";
+  }
 }
 
 function saludo(idioma) {
@@ -28,6 +39,12 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  switch(idioma){
+    case "aleman": return "Guten Tag!";
+    case "mandarin": return "Ni Hao!";
+    case "ingles": return "Hello!";
+    default: return "Hola!";
+  }
 }
 
 function colors(color) {
@@ -38,18 +55,31 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+  switch(color){
+    case "blue": return "This is blue";
+    case "red": return "This is red";
+    case "green": return "This is green";
+    case "orange": return "This is orange";
+    default: return "Color not found";
+  }
 }
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(numero===10||numero===5)
+    return true;
+  return false;
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(numero<50&&numero>20)
+    return true;
+  return false;
 }
 
 function esEntero(numero) {
@@ -60,6 +90,9 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  if(numero-Math.floor(numero)==0)
+    return true;
+  return false;
 }
 
 function fizzBuzz(numero) {
@@ -67,6 +100,17 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  var cont=0;
+  if(numero%3===0)
+    cont+=1;
+  if(numero%5===0)
+    cont+=2;
+  switch(cont){
+    case 1: return "fizz";
+    case 2: return "buzz";
+    case 3: return "fizzbuzz";
+    default: return numero;
+  }
 }
 
 function operadoresLogicos(num1, num2, num3) {
@@ -76,6 +120,15 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  if(num1<0||num2<0||num3<0)
+    return "Hay negativos";
+  else if(num1===0||num2===0||num3===0)
+    return "Error";
+  else if(num3>num1&&num3>num2)
+    return num3+1;
+  else if(num1>num2&&num1>num3)
+    return "Número 1 es mayor y positivo";
+  return false;  
 }
 
 function esPrimo(numero) {
@@ -84,6 +137,9 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  var cont=0;
+  for(var i=3;i<=numero;i++)
+    if(numero)  
 }
 
 function esVerdadero(valor){
