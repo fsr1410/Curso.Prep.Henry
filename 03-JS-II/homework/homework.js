3be06520042bc77e244d82ff88f6480991f6d5d5
fsr1410 <faq.ramos.14101999@gmail.com>
2021-12-03 14:44:08 -0300

    Resubida de 03-JS-II/homework.js

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -137,35 +137,61 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-  var cont=0;
-  for(var i=3;i<=numero;i++)
-    if(numero)  
+  if(numero<2)
+    return false;
+  for(var i=2;i<numero;i++)
+    if(numero%i==0)
+      return false;
+  return true;  
 }
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-
+  if(!!valor)
+    return "Soy verdadero";
+  return "Soy falso";
 }
 
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
-  //Escribe tu código aquí   
-  
+  //Escribe tu código aquí
+  var vec = [];
+  const NUM=6; //number to multiply
+  const MAY=60; //max value of the table
+  const REP=MAY/NUM; //number of repetitions  
+  for(var i=0;i<=REP;i++){
+    vec[i]=NUM*i;
+    console.log(NUM+" x "+i+" = "+vec[i]);
+  }
+  return vec;
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+  var cantDig=0;
+  while(numero>0){
+    numero=Math.trunc(numero/10);
+    cantDig++;
+  }
+  if(cantDig==3)
+    return true;
+  return false;
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  var cont=0;
+  do{
+    cont++;
+    numero+=5;
+  }while(cont!=8);
+  return numero;
 }
 
 
